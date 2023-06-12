013e4eaa962d32388052800962c5e687caed70f2
jhennyticona <102036237+jhennyticona@users.noreply.github.com>
2023-06-12 18:38:52 -0300

    Atualização de Cores, rename da pasta controller

--- src/suaDespensa/Menu.java
+++ src/suaDespensa/Menu.java
@@ -3,7 +3,8 @@ package suaDespensa;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import controller.Controller;
+import suaDespensa.util.Cores;
+import suaDespensa.controller.Controller;
 import suaDespensa.model.Bebida;
 import suaDespensa.model.NaoPerecivel;
 
@@ -12,24 +13,16 @@ public class Menu {
 
 		Controller produtos = new Controller();
 
-		Bebida b1 = new Bebida(produtos.gerarNumero(), "Água", 2, 2, 1);
-		b1.visualizar();
-
-		NaoPerecivel np1 = new NaoPerecivel(produtos.gerarNumero(), "Arroz", 5, 1);
-		np1.visualizar();
-
-		NaoPerecivel np2 = new NaoPerecivel(produtos.gerarNumero(), "Geleia", 3, 1);
-		np2.visualizar();
-
 		produtos.listarItens();
 
 		Scanner leia = new Scanner(System.in);
 
-		int opcao, numero, qtd, categoria, tipo,aux;
+		int opcao, numero, qtd, categoria, tipo, aux;
 		String nome;
 
 		while (true) {
 
+			System.out.println(Cores.TEXT_CYAN_BOLD+ Cores.ANSI_BLACK_BACKGROUND +"");
 			System.out.println("*****************************************************");
 			System.out.println("                                                     ");
 			System.out.println("                    SUA DESPENSA                     ");
@@ -45,7 +38,8 @@ public class Menu {
 			System.out.println("            5 - Apagar item                          ");
 			System.out.println("            6 - Sair                                 ");
 			System.out.println("                                                     ");
-			System.out.println("*****************************************************");
+			System.out.println("*****************************************************"+
+			Cores.TEXT_RESET+Cores.TEXT_CYAN_BOLD);
 			System.out.println("Entre com a opção desejada:");
 
 			try {
@@ -68,11 +62,11 @@ public class Menu {
 				System.out.println("|Cadastrar item na Sua Despensa| Digite o nome do produto: ");
 				leia.skip("\\R?");
 				nome = leia.nextLine();
-				
-				if(produtos.buscarNaCollectionNome(nome)==null) {
+
+				if (produtos.buscarNaCollectionNome(nome) == null) {
 					System.out.println("Produto não encontrado, deseja adicionar na sua Despensa (1-Sim 2-Não)?");
-					aux=leia.nextInt();
-					if(aux==1) {
+					aux = leia.nextInt();
+					if (aux == 1) {
 						System.out.println("Digite a quantidade: ");
 						qtd = leia.nextInt();
 
@@ -90,12 +84,12 @@ public class Menu {
 							produtos.criarItem(new NaoPerecivel(produtos.gerarNumero(), nome, qtd, categoria));
 						}
 					}
-					
-				}else {
-					System.out.println("Você já tem "+nome+" na sua Despensa.Para atualizar é a opção 4.");
-					
+
+				} else {
+					System.out.println("Você já tem " + nome + " na sua Despensa.Para atualizar é a opção 4.");
+
 				}
-			
+
 				break;
 			case 2:
 				System.out.println("|Listar itens na Sua Despensa|\n");
@@ -104,7 +98,7 @@ public class Menu {
 				break;
 			case 3:
 				System.out.println("|Buscar item pelo nº| Digite o nº do item desejado: ");
-				
+
 				numero = leia.nextInt();
 
 				produtos.buscarPeloNumero(numero);
@@ -129,14 +123,14 @@ public class Menu {
 
 					if (categoria == 2) {
 						System.out.println("Digite o tipo do produto (1 - Não Alcoólico ou 2 - Alcoólico): ");
-						
+
 						tipo = leia.nextInt();
-						
-						produtos.atualizarItem(new Bebida(numero,nome, qtd, categoria, tipo));
+
+						produtos.atualizarItem(new Bebida(numero, nome, qtd, categoria, tipo));
 
 					} else if (categoria == 1) {
 						produtos.atualizarItem(new NaoPerecivel(numero, nome, qtd, categoria));
-						
+
 					}
 
 				} else
